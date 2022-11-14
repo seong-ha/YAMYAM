70a878a6047c86c5f1323eee66bb3cc500e7e64b
이주훈 <leejuhoon7@naver.com>
2022-11-14 10:55:24 +0900

    2022-11-14

--- YAMYAM/src/main/java/com/mandu/yamyam/emp/web/EmpController.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/emp/web/EmpController.java
@@ -1,20 +1,14 @@
 package com.mandu.yamyam.emp.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.mandu.yamyam.emp.mapper.EmpMapper;
 
 
 @Controller
 public class EmpController {
-	@Autowired
-	EmpMapper dao;
 
 	@RequestMapping("/emp")
-	public String employee() {
+	public String emp() {
 		
 		return "common/employeeAD";
 	}
