e0ab7f3d77f4ddd9977249045268a7dc83fd6e14
아현 <lemon96_@naver.com>
2022-11-15 17:46:19 +0900

    11/15 영업 전체 메뉴 큰 틀 수정

--- YAMYAM/src/main/java/com/mandu/yamyam/ord/web/OrdController.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/ord/web/OrdController.java
@@ -6,24 +6,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OrdController {
-
+	
+	// 주문 관리
 	@RequestMapping("/odAd") 
 	public String odAd(Model model) {
 		return "order/odAd";
 	}
 	
+	// 완제품 출고 관리
 	@RequestMapping("/prdOutAd") 
 	public String prdOutAd(Model model) {
 		return "order/prdOutAd";
 	}
 	
+	// 완제품 출고 조회
 	@RequestMapping("/prdOutList") 
 	public String prdOutList(Model model) {
 		return "order/prdOutList";
 	}
 	
+	// 완제품 반품 관리
 	@RequestMapping("/prdReAd") 
 	public String prdReAd(Model model) {
 		return "order/prdReAd";
 	}
+	
+	// 완제품 재고 조회
+	@RequestMapping("/prdList")
+	public String prdList(Model model) {
+		return "order/prdList";
+	}
+	
+	// 완제품 재고 관리
+	@RequestMapping("/prdAd")
+	public String prdAd(Model model) {
+		return "order/prdAd";
+	}
+	
+	// 생산 요청 관리
+	@RequestMapping("/podOrderAd")
+	public String podOrderAd(Model model) {
+		return "order/podOrderAd";
+	}
+	
+	// 제품 안전 재고 관리
 }
