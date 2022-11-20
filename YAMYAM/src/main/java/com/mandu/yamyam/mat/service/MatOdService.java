9254fa346ab2ec54f54fafbe66fd59b632be4eae
uuuqw <uuuqw@DESKTOP-15JNEH1>
2022-11-21 01:33:37 +0900

    2022-11-21 1:31
    1)자재 발주 관리 일반 탭 남은 작업
    	추가 -> 발주코드는 insert 시 자동으로 입력되도록 처리해야함.
    	삭제 -> null 담기는 #{value}
    	저장 -> 아직 미처리
    
    2)자재 발주 관리 생산계획서용 탭 남은 작업
    	자재발주에 안전발주량 계산되어 나오도록.
    	저장 -> 자재발주가 저장되도록.

--- YAMYAM/src/main/java/com/mandu/yamyam/mat/service/MatOdService.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/mat/service/MatOdService.java
@@ -5,31 +5,40 @@ import java.util.Map;
 
 public interface MatOdService {
 	
-	// 전체조회(일반 탭)
-	public List<Map<String, Object>> matOrderList();
-	
-	// 돋보기 자재목록 모달
+	// 돋보기 자재 목록 모달
 	public List<Map<String, Object>> matList();
 	
-	// 돋보기 업체목록 모달
+	// 돋보기 업체 목록 모달
 	public List<Map<String, Object>> actList();
 	
-	// 신규생산계획조회(생산계획서용 탭)
+	// 자재 발주 관리 - 전체 조회(일반 탭)
+	public List<Map<String, Object>> matOrderList();
+	
+	// 자재 발주 관리 - 발주코드 삭제(일반 탭)
+	public int delMatOdList(MatVO vo);
+	
+	// 자재 발주 관리 - 발주상세코드 삭제(일반 탭)
+	public int delMatOddList(MatVO vo);
+	
+	// 신규 생산계획 조회(생산계획서용 탭)
 	public List<Map<String, Object>> newPlanList();
 	
 	// 신규 생산 계획서 모델 선택 모달창(생산계획서용 탭)
 	public List<Map<String, Object>> addNewPlan();
 
-	// 자재발주 전체리스트(생산계획서용 탭)
+	// 자재 발주 전체 리스트(생산계획서용 탭)
 	public List<Map<String, Object>> odMatList();
 	
-	// 자재발주 전체리스트(생산계획서용 탭)
+	// 신규 발주 전체 리스트(생산계획서용 탭)
 	public List<Map<String, Object>> newPlanInfo(MatVO vo);
 	
-	// 필요자재조회(생산계획 탭)
+	// 필요 자재 리스트(자재 발주 - 생산계획 탭)
 	public List<Map<String, Object>> needMatList(MatVO vo);
 	
-	// 자재발주 리스트(생산계획 탭)
+	// 자재 발주 리스트(자재 발주 - 생산계획 탭)
 	public List<Map<String, Object>> chkMatList(MatVO vo);
+	
+	// 발주 신청일 조회 버튼 클릭 이벤트 (자재 발주 조회 - 조건 조회 탭)
+	public List<Map<String, Object>> odListSearch(MatVO vo);
 	
 }
