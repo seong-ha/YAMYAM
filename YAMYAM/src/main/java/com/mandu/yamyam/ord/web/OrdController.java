6d68321e72873153c0cf7c9d0f2a0be20c574c9b
아현 <lemon96_@naver.com>
2022-11-21 18:05:34 +0900

    11/21 영업관리 주문서관리 TAB CRUD 작업

--- YAMYAM/src/main/java/com/mandu/yamyam/ord/web/OrdController.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/ord/web/OrdController.java
@@ -1,11 +1,50 @@
 package com.mandu.yamyam.ord.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mandu.yamyam.ord.service.OrdService;
+import com.mandu.yamyam.ord.service.OrdVO;
 
 @Controller
 public class OrdController {
+	
+	@Autowired
+	OrdService service;
+	
+	// 주문서 조회
+	@GetMapping("ordList")
+	@ResponseBody
+	public List<OrdVO> getOrdList() {
+		return service.getOrdList();
+	}
+	
+	// 주문서 등록
+	@PostMapping("/insertOrd")
+	@ResponseBody
+	public int insertOrd(@RequestBody List<OrdVO> vo) {
+		System.out.println(1213);
+		int result =  service.insertOrd(vo);
+		System.out.println(result);
+		return result;
+	}
+	
+	// 주문서 선택삭제
+	@DeleteMapping("deleteOrd")
+	@ResponseBody
+	public int deleteOrd(@RequestBody OrdVO vo) {
+		int result = service.deleteOrd(vo);
+		return result;
+	}
 	
 	// 주문 관리
 	@RequestMapping("/odAd") 
