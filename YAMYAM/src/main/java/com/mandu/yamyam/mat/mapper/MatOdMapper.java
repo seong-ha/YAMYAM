04aa698c57b3b566ce0aee37ba250090bf8897da
현성 <79891518+uuuqwe12@users.noreply.github.com>
2022-11-23 00:25:10 +0900

    2022-11-23 (#50)
    1. 자재발주관리
    2. 자재발주조회
    
    완성 ^^ /*******
    
    Co-authored-by: uuuqw <uuuqw@DESKTOP-15JNEH1>

--- YAMYAM/src/main/java/com/mandu/yamyam/mat/mapper/MatOdMapper.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/mat/mapper/MatOdMapper.java
@@ -3,18 +3,23 @@ package com.mandu.yamyam.mat.mapper;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
-
 import com.mandu.yamyam.mat.service.MatVO;
 
 public interface MatOdMapper {
-	
+	//===============================================
+	// 공통
+	//===============================================
 	// 돋보기 자재목록 모달
 	public List<Map<String, Object>> matList();
 	
 	// 돋보기 업체목록 모달
 	public List<Map<String, Object>> actList();
 	
+	
+	
+	//===============================================
+	// 자재 발주 관리
+	//===============================================
 	// 자재 발주 관리(일반 탭)  - 전체 조회
 	public List<Map<String, Object>> matOrderList();
 	
@@ -23,6 +28,9 @@ public interface MatOdMapper {
 	
 	// 자재 발주 관리 - 발주상세코드 여러건 delete(일반 탭)
 	public int delMatOdList(MatVO vo);
+	
+	// 자재 발주 관리 - 발주상세코드 여러건 delete(일반 탭)
+	public int delMatOddList(MatVO vo);
 	
 	// 자재 발주 관리 - 발주상세코드 insert(일반 탭)
 	public int insMatOdList(MatVO vo);
@@ -45,9 +53,21 @@ public interface MatOdMapper {
 	// 자재발주 리스트(생산계획 탭)
 	public List<Map<String, Object>> chkMatList(String mtrCd);
 	
+	// 신규 생산 계획서 리스트 조회버튼(자재 발주 - 생산계획 탭)
+	public List<Map<String, Object>> newPlanLookUpBtn(MatVO vo);
+	
+	//===============================================
+	// 자재 발주 조회
+	//===============================================
 	// 발주 신청일 조회 버튼 클릭 이벤트 (자재 발주 조회 - 조건 조회 탭)
 	public List<Map<String, Object>> odListSearch(MatVO vo);
+	
+	// 발주 신청일 클릭 시 이벤트 (조건 조회 탭)
+	public List<Map<String, Object>> clickOdDate(MatVO vo);
 
+	//===============================================
+	// 자재 입고 검수 관리
+	//===============================================
 	// 자재 입고 검수 관리 - 추가 모달창
 	public List<Map<String, Object>> addChkModal();
 }
