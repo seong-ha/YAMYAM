2533ed4d57cc8d0d365d683bcfd621e92acb0bd2
leejuhoon7 <105280899+leejuhoon7@users.noreply.github.com>
2022-11-28 18:00:21 +0900

    Juhoon13 (#82)
    * 2022-11-26 6시! grid selectbox 설정하는거 완료!
    
    * 2022-11-28 노트북
    
    * 2022-11-28 마지막 탭 작업중 내일 전체적으로 수정 예정
    
    Co-authored-by: Hoon <Hoon@DESKTOP-MVV881T>

--- YAMYAM/src/main/java/com/mandu/yamyam/comm/mapper/CommMapper.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/comm/mapper/CommMapper.java
@@ -14,18 +14,15 @@ public interface CommMapper {
 	// ajax 공통코드 불러오기
 	public List<CommVO> ajaxSelectComm();
 	
+	
 	// ajax 상세 공통 코드 불러오기
 	public List<CommdVO> ajaxSelectDetailComm(CommdVO commdVO);
-
 	// ajax 상세 공통 코드 등록
 	public int ajaxInsertDetailComm(CommdVO commdVO);
-	
 	// ajax 상세 공통 코드 삭제
 	public int ajaxDeleteDetailComm(CommdVO commdVO);
-	
 	// ajax 공통 코드 등록
 	public int ajaxInsertComm(CommVO commVO);
-	
 	// 상세 공통 코드, 이름 불러오기(성하)
 	public List<CommdVO> getCommdCdNm(String commCd);
 	
@@ -36,15 +33,29 @@ public interface CommMapper {
 	public List<ActVO> ajaxSelectModalAct();
 	// ajax 자재 - 자재 단건 정보 가져오기
 	public MtrVO ajaxSelectMtrInfo(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 등록하기
+	public int ajaxInsertMtr(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 수정하기
+	public int ajaxUpdateMtr(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 삭제하기
+	public int ajaxDeleteMtr(MtrVO mtrVO);
 	
 	
 	
 	// ajax 거래처 코드 불러오기
 	public List<ActVO> ajaxSelectAct();
+	// ajax 구매 거래처 등록하기
+	public int ajaxInsertAct(ActVO actVO);
+	// ajax 구매 거래처 수정하기
+	public int ajaxUpdateAct(ActVO actVO);
 	
 	
 	// ajax 완제품 코드 불러오기
 	public List<PrdVO> ajaxSelectPrd(PrdVO prdVO);
+	// ajax 완제품 코드 등록하기
+	public int ajaxInsertPrd(PrdVO prdVO);
+	// ajax 완제품 코드 삭제하기
+	public int ajaxDeletePrd(PrdVO prdVO);
 	
 	
 	// ajax BOM 코드 불러오기
--- YAMYAM/src/main/java/com/mandu/yamyam/comm/service/CommService.java
+++ YAMYAM/src/main/java/com/mandu/yamyam/comm/service/CommService.java
@@ -10,16 +10,12 @@ public interface CommService {
 	
 	// ajax 상세 공통 코드 불러오기
 	public List<CommdVO> ajaxSelectDetailComm(CommdVO commdVO);
-
 	// ajax 상세 공통 코드 등록
 	public int ajaxInsertDetailComm(List<CommdVO> commdVO); 
-	
 	// ajax 상세 공통 코드 삭제
 	public int ajaxDeleteDetailComm(List<CommdVO> commdVO);
-	
 	// ajax 공통 코드 등록
 	public int ajaxInsertComm(CommVO commVO);
-	
 	// 상세 공통 코드, 이름 불러오기(성하)
 	public List<CommdVO> getCommdCdNm(String commCd);
 
@@ -31,14 +27,28 @@ public interface CommService {
 	public List<ActVO> ajaxSelectModalAct();
 	// ajax 자재 - 자재 단건 정보 가져오기
 	public MtrVO ajaxSelectMtrInfo(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 등록하기
+	public int ajaxInsertMtr(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 수정하기
+	public int ajaxUpdateMtr(MtrVO mtrVO);
+	// ajax 자재 - 자재 정보 삭제하기
+	public int ajaxDeleteMtr(List<MtrVO> mtrVO);
 	
 	
 	// ajax 거래처 코드 불러오기
 	public List<ActVO> ajaxSelectAct();
+	// ajax 구매 거래처 등록하기
+	public int ajaxInsertAct(ActVO actVO);
+	// ajax 구매 거래처 수정하기
+	public int ajaxUpdateAct(ActVO actVO);
 	
 	
 	// ajax 완제품 코드 불러오기
 	public List<PrdVO> ajaxSelectPrd(PrdVO proVO);
+	// ajax 완제품 코드 등록하기
+	public int ajaxInsertPrd(PrdVO prdVO);
+	// ajax 완제품 코드 삭제하기
+	public int ajaxDeletePrd(List<PrdVO> prdVO);
 	
 	
 	// ajax BOM 코드 불러오기
